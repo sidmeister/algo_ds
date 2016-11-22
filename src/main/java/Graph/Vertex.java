6468bfff01b0faec4e3a00abfb04f5370eeffd89
Siddharth Rathi <srathi@hotwire.com>
2016-11-22 11:01:19 -0800

    added graph

--- src/main/java/Graph/Vertex.java
+++ src/main/java/Graph/Vertex.java
@@ -4,4 +4,18 @@ package Graph;
  * Created by srathi on 11/20/16.
  */
 public class Vertex {
+
+    Character label;
+    Boolean visited;
+
+    public Vertex()
+    {
+        visited = false;
+    }
+
+    public Vertex(Character label)
+    {
+        this.label = label;
+        visited = false;
+    }
 }
